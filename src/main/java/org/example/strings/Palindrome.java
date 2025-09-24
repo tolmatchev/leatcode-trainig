39d62b080c7614d08a130d243215ffc68c45a189
Guzime <forblachok@yandex.ru>
2025-09-24 18:03:14 +0300

    rename class

--- src/main/java/org/example/strings/Palindrome.java
+++ src/main/java/org/example/strings/Palindrome.java
@@ -1,6 +1,6 @@
 package org.example.strings;
 
-public class Paindrome {
+public class Palindrome {
     /**
      * A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
      *
--- src/test/java/org/example/strings/AnagramTest.java
+++ src/test/java/org/example/strings/AnagramTest.java
@@ -7,42 +7,42 @@ import static org.junit.jupiter.api.Assertions.*;
 class AnagramTest {
     @Test
     public void test1() {
-        Paindrome string5Task = new Paindrome();
+        Palindrome string5Task = new Palindrome();
         boolean palindrome = string5Task.isPalindrome("A man, a plan, a canal: Panama");
         assertTrue(palindrome);
     }
 
     @Test
     public void test2() {
-        Paindrome string5Task = new Paindrome();
+        Palindrome string5Task = new Palindrome();
         boolean palindrome = string5Task.isPalindrome(".");
         assertTrue(palindrome);
     }
 
     @Test
     public void test3() {
-        Paindrome string5Task = new Paindrome();
+        Palindrome string5Task = new Palindrome();
         boolean palindrome = string5Task.isPalindrome("0P");
         assertFalse(palindrome);
     }
 
     @Test
     public void test4() {
-        Paindrome string5Task = new Paindrome();
+        Palindrome string5Task = new Palindrome();
         boolean palindrome = string5Task.isPalindrome2("A man, a plan, a canal: Panama");
         assertTrue(palindrome);
     }
 
     @Test
     public void test5() {
-        Paindrome string5Task = new Paindrome();
+        Palindrome string5Task = new Palindrome();
         boolean palindrome = string5Task.isPalindrome2(".");
         assertTrue(palindrome);
     }
 
     @Test
     public void test6() {
-        Paindrome string5Task = new Paindrome();
+        Palindrome string5Task = new Palindrome();
         boolean palindrome = string5Task.isPalindrome2("0P");
         assertFalse(palindrome);
     }
